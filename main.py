037462a35374da91a8500847532af9bff1cef888
bgraver <graver.brandon@gmail.com>
2020-09-04 15:13:22 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import json
+
+with open('keys.json') as f:
+    keys = json.load(f)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(keys['discord']['ClientID'])
